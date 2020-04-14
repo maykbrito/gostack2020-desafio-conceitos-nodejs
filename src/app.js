94bddaf0005bbb66019b64f745d7d964e7594d4f
Mayk Brito <maykbrito@gmail.com>
2020-04-14 08:44:55 -0400

    add CRUD and Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,56 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json( repositories )
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const project = { id: uuid(), title, url, techs, likes: 0}
+
+  repositories.push(project)
+
+  return response.json( project )
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const { id } = request.params
+
+  let repository = repositories.find( repository => repository.id === id )
+  if (!repository)
+    return response.status(400).json({ error: "Repository not found." })
+  
+  repository = {...repository, title, url, techs}
+
+  return response.json( repository )
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id )
+  if (repositoryIndex < 0)
+    return response.status(400).json({ error: "Repository not found." })
+
+  repositories.splice(repositoryIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repository = repositories.find( repository => repository.id === id )
+  
+  if (!repository)
+    return response.status(400).json({ error: "Repository not found." })
+  
+  repository.likes += 1
+
+  return response.json(repository)
+  
 });
 
 module.exports = app;
